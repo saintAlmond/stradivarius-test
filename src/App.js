5283ac2b1314c84005c0e555f43f55641b1ad255
saintAlmond <ialmen1988@gmail.com>
2021-01-11 19:35:43 +0100

    create carrousel with pure-react-carousel

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import React from 'react';
+import styled, { css } from 'styled-components';
+import { 
+  CarrouselProvider, 
+  Slider, 
+  Slide,
+  ButtonNext,
+  ButtonBack,
+} from "pure-react-carousel";
+import "pure-react-carousel/dist/react-carousel.es.css";
+// import Carrousel from './components/Carrousel';
+
+const PageContainer = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items:center;
+  `
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <PageContainer className="App">
+      <SliderWrapper>
+        
+      </SliderWrapper>
+    </PageContainer>
   );
 }
 
